28ec7cb06671e245dcc8f038f48fdad88c97e2ed
Wahid8111 <wahid.mensia.m2024@ihec.ucar.tn>
2026-05-12 16:59:16 +0100

    Force dynamic rendering on invite page to fix build error

--- app/(auth)/invite/page.tsx
+++ app/(auth)/invite/page.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+export const dynamic = "force-dynamic"
+
+
 import { Suspense, useEffect, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import Image from "next/image"
